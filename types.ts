05d739c340d44e9ac3bfc8703095cd773d504e63
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-07 23:52:59 -0300

    Typescript types

---
+++ types.ts
@@ -0,0 +1,20 @@
+// boolean (true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = `isOpen: ${isOpen}`
+
+//number (int, float, hexa, binario)
+let total: number
+total = 3
+total = 40.99
+total = 0xff0
+
+// array (type[])
+let items: number[]
+items = [1, 2, 3, 4]
+
+let letters: Array<string>
+letters = ['a', 'b', 'c']
