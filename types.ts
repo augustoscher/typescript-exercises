6efb4d99b9ee007d23b179962f01f77021b4ea9c
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-08 00:08:18 -0300

    Typescript types

--- types.ts
+++ types.ts
@@ -18,3 +18,42 @@ items = [1, 2, 3, 4]
 
 let letters: Array<string>
 letters = ['a', 'b', 'c']
+
+// tuple - array com tamanho e tipo definidos
+let tup: [string, number, boolean]
+tup = ['a', 1, true]
+
+// enum
+enum Colors {
+  white = '#fff',
+  black = '#000'
+}
+
+// any (qualquer coisa - não é legal)
+let coisa: any
+coisa = true
+coisa = 1
+coisa = 'S'
+
+// void (vazio) não retorna nada 
+// function logger(): void {
+//   console.log('foo')
+// }
+function logger() {
+  console.log('foo');
+}
+
+// null | undefined (mesma coisa)
+type Bla = string | undefined
+
+// never (nunca vai retornar)
+// erros nunca retornam nada, só lançam exceção.
+function error(): never {
+  throw new Error('asd');
+}
+
+// object (tudo aqui que não é um tipo primitivo)
+let obj: object
+obj = {
+  title: "adaasjdoi"
+}
