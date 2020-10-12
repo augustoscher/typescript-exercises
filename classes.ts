1bccbb2c5a0e0f37c5d2eaf8b3a4e1f009cd8842
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-12 13:22:28 -0300

    Classes

--- classes.ts
+++ classes.ts
@@ -1,6 +1,6 @@
 class UserAccount {
-  name: string;
-  age: number;
+  public name: string; //modificador default
+  protected age: number; //só funciona no escopo dentro da classe ou de subclasses.
 
   constructor(name: string, age: number){
     this.name = name;
@@ -12,6 +12,25 @@ class UserAccount {
   }
 }
 
+class CharAccount extends UserAccount {
+  private nickname: string; //não fica visível para nada fora do escopo da classe.
+  readonly level: number; //permite somente ler fora do escopo da classe.
+
+  constructor(name: string, age: number, nickname: string, level: number){
+    super(name, age);
+    this.nickname = nickname;
+    this.level = level;
+  }
+
+  get getLevel() {
+    return this.level
+  }
+}
+
 const as = new UserAccount('Augusto', 29);
 console.log(as);
-console.log(as.logDetails())
+as.logDetails()
+
+const ca = new CharAccount('A', 29, 'ascher', 100);
+console.log('CharAccount: ', ca);
+ca.logDetails();
