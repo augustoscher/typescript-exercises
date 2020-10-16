6fa6606203f2e0b9043f5dba1bedf6be3c01df8c
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-16 09:02:00 -0300

    Type utilities

--- type-utilities.ts
+++ type-utilities.ts
@@ -4,12 +4,25 @@ type Todo = {
   completed: boolean;
 }
 
-const todo: Todo = {
+// make all properties readonly
+const todo: Readonly<Todo> = {
   title: "Assistir Dark",
   description: "Dizem que é legal",
   completed: false,
 }
 
 console.log(todo);
-todo.completed = true;
-console.log(todo);
+// todo.completed = true; not allowed
+
+// returns another object spreading todo's on object
+// Partial make all properties in T optionals
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => {
+  return { 
+    ...todo,
+    ...fieldsToUpdate
+  }
+}
+
+const todo2: Todo = updateTodo(todo, { completed: true })
+
+// pick
