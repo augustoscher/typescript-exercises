27668a6f7d715c57d74c927edb976fbaa294ba71
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-04 10:24:16 -0300

    feat: LRUCache implementation typescript

--- hacking/src/lru-cache.ts
+++ hacking/src/lru-cache.ts
@@ -1,7 +1,8 @@
 type Doc = {
-  title: string
-  body: string
+  id: number
   artifact: string
+  title?: string
+  body?: string
 }
 
 class LRUCache {
@@ -14,15 +15,23 @@ class LRUCache {
   }
 
   set(key: string, value: Doc): void {
-
+    //if we're at capacity, we need to remove the least-recently-used item:
+    if (this.cache.size >= this.capacity) {
+      const leastRecentlyUsedKey = this.cache.keys().next().value;
+      this.cache.delete(leastRecentlyUsedKey);
+    }
+    // even so, we need to reinsert the value in the cache to make it more recently used.
+    this.cache.delete(key);
+    this.cache.set(key, value)
   }
 
   get(key: string): Doc {
-
-    return {
-      title: 'asd',
-      body: 'asd',
-      artifact: 'asd'
+    if (this.cache.has(key)) {
+      // we need to make it more recently used
+      const value = this.cache.get(key);
+      this.cache.delete(key);
+      this.cache.set(key, value);
+      return value;
     }
   }
 
@@ -30,3 +39,23 @@ class LRUCache {
     console.log(this.cache)
   }
 }
+
+const cache = new LRUCache(5);
+cache.set('a', { id: 1, artifact: 'JURISPRUDENCIA' });
+cache.set('b', { id: 2, artifact: 'JURISPRUDENCIA' });
+cache.set('c', { id: 3, artifact: 'JURISPRUDENCIA' });
+cache.log(); // a is the least-recently used
+
+console.log(cache.get("b"));
+cache.log(); // /b is the more recently used
+
+cache.set("a", { id: 111, artifact: 'JURISPRUDENCIA' }); // a is the more recent used
+cache.log();
+
+cache.set('d', { id: 4, artifact: 'JURISPRUDENCIA' });
+cache.set('e', { id: 5, artifact: 'JURISPRUDENCIA' });
+cache.log();
+
+// c is the least-recently used, so if we add a new value, it should be removed:
+cache.set('f', { id: 6, artifact: 'JURISPRUDENCIA' });
+cache.log();
